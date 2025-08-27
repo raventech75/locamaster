7e0a3adfd65a20894374b1b1e539157bac8cf511
Feridun <feridun@macbookpro.home>
2025-08-27 12:29:51 +0200

    Add anamytics

--- postcss.config.js
+++ postcss.config.js
@@ -4,3 +4,17 @@ module.exports = {
     autoprefixer: {},
   },
 }
+
+// pages/_app.tsx
+import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/next'
+import '../styles/globals.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
+}
